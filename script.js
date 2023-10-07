b558ae6edc5cf1bbdb0aafac7e869becbdcfe4f7
Peeyush Tyagi <peeyushtyagi99@gmail.com>
2023-10-07 16:57:54 +0530

    locomotive

---
+++ script.js
@@ -0,0 +1,10 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+function circleMouseFollower(){
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+circleMouseFollower();
